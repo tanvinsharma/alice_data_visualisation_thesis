37d33f7fe0e7d450976177e4a93ebdd825c5f69e
tanvinsharma <tanvin2000s@gmail.com>
2021-12-05 11:13:43 +0100

    added docker support

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This repository contains scripts for visualising data from the CERN ALICE experi
 ## Installation
 Assuming the user has Python and pip installed, we can use the following steps to run this project
 
-### Step 1: Creating venv
+### Method 1: Creating venv
 To start off, we will create a virtual environment and download all dependencies in there
 To create it, go to the designated folder in the terminal, and run the following command:
 ```
@@ -26,3 +26,10 @@ To run **jupyter notebook**, run the command
 jupyter notebook
 ```
 This should automatically open your browser to jupyter and from there on you can access the document and run the code
+
+### Method 2: Using Docker
+An alternative approach is to pull the docker image and run it
+```
+docker pull tanvinsharma/new_project
+docker run -p 8888:8888 tanvinsharma/new_project
+```
--- test.py
+++ test.py
@@ -3,11 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from pythreejs import *
-from IPython.display import display
-from ipywidgets import VBox, HBox, Checkbox, jslink
-from pythreejs._example_helper import use_example_model_ids
-use_example_model_ids()
 
 with open('/Users/tanvinsharma/Desktop/cern thesis/tracks1635345855523.json') as file:
 	db = json.load(file)
@@ -51,34 +46,12 @@ for i in range(c):
 		ax.plot(x_values[i], y_values[i], z_values[i], 'b')
 	if charge_value == -1:
 		ax.plot(x_values[i], y_values[i], z_values[i], 'r')
-	if charge_value == 0:
-		no_of_black+=1
-		ax.plot(x_values[i], y_values[i], z_values[i], 'k')
+	# if charge_value == 0:
+	# 	no_of_black+=1
+	# 	ax.plot(x_values[i], y_values[i], z_values[i], 'k')
 print(no_of_red)
 print(no_of_blue)
 print(no_of_black)
 # uncomment to see plot
 plt.show()
 
-
-# size = 4
-# linesgeom = Geometry(vertices=[[0, 0, 0],
-#                                     [size, 0, 0],
-#                                     [0, 0, 0],
-#                                     [0, size, 0],
-#                                     [0, 0, 0],
-#                                     [0, 0, size]],
-#                           colors = ['red', 'red', 'green', 'green', 'white', 'orange'])
-# lines = Line(geometry=linesgeom, 
-#              material=LineBasicMaterial(linewidth=5, vertexColors='VertexColors'), 
-#              type='LinePieces',
-#             )
-# scene = Scene(children=[
-#     lines,
-#     DirectionalLight(color='#ccaabb', position=[0,10,0]),
-#     AmbientLight(color='#cccccc'),
-#     ])
-# c = PerspectiveCamera(position=[10, 10, 10])
-# renderer = Renderer(camera=c, background='black', background_opacity=1, scene=scene, controls=[OrbitControls(controlling=c)],
-#                     width=400, height=400)
-# display(renderer)
